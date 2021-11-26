17372a0603050b07bef0fb7648b3cbe13356ee77
salihkoc <salihkoc-e@users.noreply.github.com>
2021-11-26 21:26:04 +0300

    v1

--- oop2/NewFolder1/urunSepeti.cs
+++ oop2/NewFolder1/urunSepeti.cs
@@ -9,10 +9,20 @@ namespace oop2.NewFolder1
     class urunSepeti
     {
         public int sepetID { get; set; }
+        public string urunAdi { get; set; }
         public int musteriID { get; set; }
 
         public List<urun> urunlerListesi { get; set; } = new List<urun>();
 
+        public void dogumGunuHediyesiEkle()
+        {
+            urun hediye = new urun()
+            {
+                urunAdi = "gofret";
+            urunID = 300;
+            urunKatagori = urunKatagori.gıda
+            };
+        }
 
     }
 }
